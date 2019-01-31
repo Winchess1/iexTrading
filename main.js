50a0e4dc5a9bdf58f5c8030d3648efb6287ac364
Winchess1 <ikneazevmd@gmail.com>
2019-01-31 13:09:38 -0600

    major batch

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const getIextrading = require('./stockfilter');
 const extractor = require('./stockHandler');
-const alertCounter = require('./alertCounter');
+//const temp = require('./temp');
+//const history = require('./historySaveModule');
+const bodyParser = require('body-parser');
 
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
@@ -20,18 +23,63 @@ app.get('/', (req, res) => {
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.get('/:id', (req, res) => {
+app.get('/stockhandler/:id', (req, res) => {
+    console.log(req.body);
+    extractor.extractor(function (callback) {
 
-    extractor.extractor(function (callback) {       
-console.log(alertCounter);
 
-     res.render('template', { stockData: callback, fiveMinuteChart: callback,alertCounter:alertCounter },  
-     console.log("page loaded " + (typeof (callback) !== 'undefined' ? '100%' : 'false')));
-              
+        res.render('template', {
+            stockData: callback,
+            fiveMinuteChart: callback,
+            alertCounter: callback,
+            historyDate: req.body,
+        },
+            console.log("page loaded " + (typeof (callback) !== 'undefined' ? '100%' : 'false')));
+
     }, req.params.id);
 
 
 });
+/* app.get('/history', (req, res) => {
+    history.history(function(callback){
+       
+        res.send(callback);
+        res.end();
+
+    })
+ 
+
+
+}); */
+
+app.post('/stockhandler/:id', (req, res) => {
+
+console.log(typeof(req.body.date));
+    extractor.extractor(function (callback) {
+
+        res.render('template', {
+            stockData: callback,
+            fiveMinuteChart: callback,
+            alertCounter: callback,
+            historyDate: req.body
+        },
+            console.log("page loaded " + (typeof (callback) !== 'undefined' ? '100%' : 'false')));
+
+    }, req.params.id, req.body);
+
+})
+/* app.get('/stockhandler', (req, res) => {
+    temp.temp(function(callback){
+       
+        res.send(callback);
+        res.end();
+
+    })
+ 
+
+
+}); */
+
 
 
 
